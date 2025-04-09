8fae10b3abef011c38919568e5a7a927d1fd438b
Jude Flourish <judeflourish01@gmail.com>
2025-04-09 20:39:34 +0100

    Updated string method

---
+++ switch.js
@@ -0,0 +1,19 @@
+let desireFriut = 'apple';
+
+switch (desireFriut) {
+    case "mango":
+        console.log("I do not want mango")
+        break;
+
+    case "pawpaw":
+        console.log("I do not want pawpaw")
+        break;
+
+    case "oranges":
+        console.log("I want oranges")
+        break;
+
+    default:
+    console.log("come back tomorrow")
+    break;
+}
---
+++ tenary.js
@@ -0,0 +1,4 @@
+
+let age = Number(prompt("enter your age"))
+
+age >= 18 ? console.log('Buy a deink') : console.log("We don't sale to under age")
